309533b93a15c3aa9c3915ddcbec20c485e6c17a
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-04-08 19:09:07 -0700

    finished basic challenges

---
+++ server.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const fetch = require('node-fetch');
+
+const schema = buildSchema(`
+  enum Unit {
+    standard
+    metric
+    imperial
+  }
+
+  type Weather {
+    temperature: Float
+    description: String
+    feelsLike: Float
+    tempMin: Float
+    tempMax: Float
+    status: Int
+    message: String
+  }
+
+  type Query {
+    getWeather(zip: Int!, unit: Unit): Weather
+  }
+`)
+
+const root = {
+  getWeather: async ({ zip, unit }) => {
+    const units = { standard: '', metric: 'metric', imperial: 'imperial' }
+
+    const API = process.env.OPEN_WEATHER_API_KEY
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${API}&units=${units[unit]}`
+
+    const res = await fetch(url)
+    const json = await res.json()
+
+    // console.log(json)
+
+    const status = parseInt(json.cod)
+
+    if (status != 200) {
+      return { status: status, message: json.message }
+    } else {
+
+      const temperature = json.main.temp
+      const description = json.weather[0].description
+      const feelsLike = json.weather[0].feels_like
+      const tempMin = json.weather[0].tempMin
+      const tempMax = json.weather[0].tempMax
+
+      return {
+        temperature: temperature,
+        description: description,
+        feelsLike: feelsLike,
+        tempMin: tempMin,
+        tempMax: tempMax,
+        status: status
+        }
+
+    }
+  }
+
+}
+
+
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+  console.log(`Running on ${port}  http://localhost:${port}/graphql`)
+})
